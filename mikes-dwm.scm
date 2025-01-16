c69e955f0d042d1c6e43a43dbb2826d58b9f0d11
Thomas Williams <mikew2801@gmail.com>
2025-01-16 19:37:44 +0100

    Add files via upload

---
+++ mikes-dwm.scm
@@ -0,0 +1,88 @@
+(define-module (gnu packages mikes-dwm)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages crates-io)
+  #:use-module (gnu packages crypto)
+  #:use-module (gnu packages compression)
+  #:use-module (gnu packages cups)
+  #:use-module (gnu packages fonts)
+  #:use-module (gnu packages fontutils)
+  #:use-module (gnu packages gawk)
+  #:use-module (gnu packages gnome)
+  #:use-module (gnu packages gtk)
+  #:use-module (gnu packages image)
+  #:use-module (gnu packages imagemagick)
+  #:use-module (gnu packages libbsd)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages mpd)
+  #:use-module (gnu packages ncurses)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages shells)
+  #:use-module (gnu packages webkit)
+  #:use-module (gnu packages xorg)
+  #:use-module (guix build-system cargo)
+  #:use-module (guix build-system glib-or-gtk)
+  #:use-module (guix build-system gnu)
+  #:use-module (guix download)
+  #:use-module (guix gexp)
+  #:use-module (guix git-download)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix utils)
+  #:use-module (guix packages))
+
+(define-public dwm
+  (package
+    (name "dwm")
+    (version "6.5")
+    (source (origin
+             (method url-fetch)
+             (uri "https://github.com/mikew2801/dwm/blob/main/dwm-6.5.tar.gz")
+             (sha256
+              (base32 "0w15ka89fl3v597karrkpzxf3ancz670nq31qxc6iwg7flbz7f7x"))))
+    (build-system gnu-build-system)
+    (arguments
+     `(#:tests? #f
+       #:make-flags (list (string-append "FREETYPEINC="
+                                         (assoc-ref %build-inputs "freetype")
+                                         "/include/freetype2"))
+       #:phases
+       (modify-phases %standard-phases
+         (replace 'configure
+           (lambda _
+             (substitute* "Makefile" (("\\$\\{CC\\}") "gcc"))
+             #t))
+        (replace 'install
+          (lambda* (#:key outputs #:allow-other-keys)
+            (let ((out (assoc-ref outputs "out")))
+              (invoke "make" "install"
+                      (string-append "DESTDIR=" out) "PREFIX="))))
+        (add-after 'build 'install-xsession
+          (lambda* (#:key outputs #:allow-other-keys)
+            ;; Add a .desktop file to xsessions.
+            (let* ((output (assoc-ref outputs "out"))
+                   (xsessions (string-append output "/share/xsessions")))
+              (mkdir-p xsessions)
+              (with-output-to-file
+                  (string-append xsessions "/dwm.desktop")
+                (lambda _
+                  (format #t
+                    "[Desktop Entry]~@
+                     Name=dwm~@
+                     Comment=Dynamic Window Manager~@
+                     Exec=~a/bin/dwm~@
+                     TryExec=~@*~a/bin/dwm~@
+                     Icon=~@
+                     Type=Application~%"
+                    output)))
+              #t))))))
+    (inputs
+     (list freetype libx11 libxft libxinerama))
+    (home-page "https://dwm.suckless.org/")
+    (synopsis "Dynamic window manager")
+    (description
+     "dwm is a dynamic window manager for X.  It manages windows in tiled,
+monocle and floating layouts.  All of the layouts can be applied dynamically,
+optimising the environment for the application in use and the task performed.")
+    (license license:x11)))
+
+dwm
